89e6f1ae07901d582afeabf5301b889d6d17120f
fredys202 <fredys202@live.com>
2018-03-18 17:44:27 -0400

    Error was fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_18_155615) do
+ActiveRecord::Schema.define(version: 2018_03_18_200130) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user"
+    t.integer "user_id"
     t.index ["user"], name: "index_pins_on_user"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
